5df291d9894211c8546b74af7604bd9526789e22
nohsangwoo <nsgr12@naver.com>
2021-07-25 19:15:04 +0900

    details...

--- components/InsertionSort.tsx
+++ components/InsertionSort.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { range, shuffle } from 'lodash';
 
 const InsertionSortContainer = styled.div``;
@@ -24,6 +24,35 @@ const Button = styled.button`
   font-size: 40px;
 `;
 
+const Index = styled.div<{ index: string; translateXValue: number }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 20px;
+  background-color: blue;
+  color: white;
+  /* transform: translateX(40px); */
+  ${props =>
+    props.index === 'i'
+      ? css`
+          background-color: yellowgreen;
+          color: black;
+        `
+      : css`
+          background-color: blue;
+          color: white;
+        `}
+`;
+
+const RunningState = styled.div`
+  font-size: 24px;
+  color: black;
+  font-weight: 600;
+`;
+
+// end of styled components
+
 const SIZE = 30;
 const getArr = () => shuffle(range(1, SIZE + 1));
 
@@ -33,31 +62,37 @@ const swap = (arr: number[], a: number, b: number) => {
   arr[b] = temp;
 };
 
-const delaySetArr = (
-  arr: number[],
-  setArr: (value: React.SetStateAction<number[]>) => any
-) => {
+type TSetArr = (value: React.SetStateAction<number[]>) => any;
+type TSetPosition = React.Dispatch<React.SetStateAction<number>>;
+
+const delaySetArr = (arr: number[], setArr: TSetArr) => {
   return new Promise(resolve => {
     setArr([...arr]);
-    setTimeout(() => resolve(resolve), 10);
+    setTimeout(() => resolve(resolve), 100);
   });
 };
 
 const sort = async (
   arr: number[],
-  setArr: (value: React.SetStateAction<number[]>) => any
+  setArr: TSetArr,
+  setPositionI: TSetPosition,
+  setPositionJ: TSetPosition
 ) => {
   // https://en.wikipedia.org/wiki/Insertion_sort
+
   let i = 1;
   while (i < arr.length) {
     let j = i;
+    setPositionI(j);
     while (j > 0 && arr[j - 1] > arr[j]) {
       // swap A[j] and A[j-1]
       swap(arr, j, j - 1);
       await delaySetArr(arr, setArr);
       j = j - 1;
+      setPositionJ(j);
     }
     i = i + 1;
+    setPositionI(i);
   }
 };
 
@@ -101,15 +136,19 @@ const BarPresent = ({ arr }: Props) => {
 // main
 const InsertionSort = (): JSX.Element => {
   const [arr, setArr] = useState<number[]>([]);
+  const [positionI, setPositionI] = useState<number>(1);
+  const [positionJ, setPositionJ] = useState<number>(1);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
 
   const handleShuffle = () => {
     setArr(getArr());
   };
 
   const handleSort = async (arr: number[]) => {
+    setIsRunning(true);
     const sorted = [...arr];
     try {
-      await sort(sorted, setArr);
+      await sort(sorted, setArr, setPositionI, setPositionJ);
       alert('Sorted!');
     } catch (e) {
       console.log(e.message);
@@ -125,10 +164,25 @@ const InsertionSort = (): JSX.Element => {
       <Board>
         <BarPresent arr={arr} />
       </Board>
+      <Index
+        index={'i'}
+        style={{ transform: `translateX(${positionI * 20}px)` }}
+        translateXValue={positionI}
+      >
+        i
+      </Index>
+      <Index
+        index={'j'}
+        style={{ transform: `translateX(${positionJ * 20}px)` }}
+        translateXValue={positionJ}
+      >
+        j
+      </Index>
 
       <ButtonBox>
-        <Button onClick={handleShuffle}>shuffle</Button>
-        <Button onClick={() => handleSort(arr)}>sort</Button>
+        {!isRunning && <Button onClick={handleShuffle}>shuffle</Button>}
+        {!isRunning && <Button onClick={() => handleSort(arr)}>sort</Button>}
+        {isRunning && <RunningState>Running...</RunningState>}
       </ButtonBox>
     </InsertionSortContainer>
   );
